9b0e95e6f2fd6457afda84b03e81b8f5fc6ccc43
hank-der-hafenarbeiter <kevinkielholz@web.de>
2015-10-02 03:27:30 +0200

    Individual steps can now be inspected

---
+++ sftreenode.cpp
@@ -0,0 +1,5 @@
+#include "sftreenode.h"
+
+SFTreeNode::SFTreeNode()
+{
+}
---
+++ sftreenode.h
@@ -0,0 +1,10 @@
+#ifndef SFTREENODE_H
+#define SFTREENODE_H
+
+class SFTreeNode
+{
+public:
+    SFTreeNode();
+};
+
+#endif // SFTREENODE_H
